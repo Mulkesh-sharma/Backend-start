fc3aa432f23bd78d57667e974ca4b8c24d47c6c8
Mulkesh Sharma <155559312+Mulkesh-sharma@users.noreply.github.com>
2025-11-10 09:44:43 +0530

    Modify MongoDB connection string
    Updated MongoDB connection string to remove credentials.

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://mulkeshshaema:Ms123456@sharma.vctfu5b.mongodb.net/');
+    await mongoose.connect('mongodb+srv://.vctfu5b.mongodb.net/');
     console.log('✅ MongoDB Connected Successfully!');
   } catch (err) {
     console.error('❌ MongoDB Connection Failed:', err.message);
